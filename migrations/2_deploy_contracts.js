650e779096406a8624110581b98d032a73ba9bb8
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2020-10-27 20:58:26 +0700

    Fix

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,5 @@
-const MyERC20 = artifacts.require('./MyERC20');
-const BigNumber = require('bignumber.js');
+const Start = artifacts.require('./Start');
 
 module.exports = function(deployer) {
-    return deployer.deploy(MyERC20, 'CipherCoin', 'CC', 18, (new BigNumber(31337).multipliedBy(1e+18)).toString(10));
+    return deployer.deploy(Start);
 };

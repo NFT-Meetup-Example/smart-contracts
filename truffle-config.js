8521c4e40bc2855eb0e330e8cad38ac79c444671
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2020-12-13 00:05:30 +0700

    Added coverage

--- truffle-config.js
+++ truffle-config.js
@@ -1,7 +1,7 @@
-const HDWalletProvider = require('truffle-hdwallet-provider')
-const NonceTrackerSubprovider = require('web3-provider-engine/subproviders/nonce-tracker')
-const config = require('config')
-const TestRPC = require('ganache-cli')
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const NonceTrackerSubprovider = require('web3-provider-engine/subproviders/nonce-tracker');
+const config = require('config');
+const TestRPC = require('ganache-cli');
 
 module.exports = {
     networks: {
@@ -11,11 +11,11 @@ module.exports = {
         },
         rinkeby: {
             provider: function () {
-                let w = new HDWalletProvider(config.get('rinkeby.truffle.privateKey'), config.get('rinkeby.blockchain.rpc'))
-                let nonceTracker = new NonceTrackerSubprovider()
-                w.engine._providers.unshift(nonceTracker)
-                nonceTracker.setEngine(w.engine)
-                return w
+                let w = new HDWalletProvider(config.get('rinkeby.truffle.privateKey'), config.get('rinkeby.blockchain.rpc'));
+                let nonceTracker = new NonceTrackerSubprovider();
+                w.engine._providers.unshift(nonceTracker);
+                nonceTracker.setEngine(w.engine);
+                return w;
             },
             network_id: config.get('rinkeby.blockchain.networkId'),
             gas: 4000000,
@@ -23,17 +23,18 @@ module.exports = {
         },
         mainnet: {
             provider: function () {
-                let w = new HDWalletProvider(config.get('mainnet.truffle.privateKey'), config.get('mainnet.blockchain.rpc'))
-                let nonceTracker = new NonceTrackerSubprovider()
-                w.engine._providers.unshift(nonceTracker)
-                nonceTracker.setEngine(w.engine)
-                return w
+                let w = new HDWalletProvider(config.get('mainnet.truffle.privateKey'), config.get('mainnet.blockchain.rpc'));
+                let nonceTracker = new NonceTrackerSubprovider();
+                w.engine._providers.unshift(nonceTracker);
+                nonceTracker.setEngine(w.engine);
+                return w;
             },
             network_id: config.get('mainnet.blockchain.networkId'),
             gas: 2000000,
             gasPrice: 60000000000
         },
     },
+    plugins: ["solidity-coverage"],
     compilers: {
         solc: {
             version: '^0.6.0',
